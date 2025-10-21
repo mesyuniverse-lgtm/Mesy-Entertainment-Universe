d3730e16c251de84fd7e82e912506f6d0b4de619
MESY-UNIVERSE <mesy.universe@gmail.com>
2025-10-21 20:20:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3890599599-53972",
+  "appId": "1:73463325437:web:236dc1da097d8a1a07d494",
+  "apiKey": "AIzaSyAI0rs2LasnbJlq6aeDTJ4HxJzQW2-oDEs",
+  "authDomain": "studio-3890599599-53972.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "73463325437"
+};
