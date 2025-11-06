0ea6a3c24582a63fab4da80fe9ad868d9f11c5b5
MESY-UNIVERSE <mesy.universe@gmail.com>
2025-11-06 01:11:18 +0000

    ยังไม่เปลี่ยนเลยค่ะ

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -89,7 +89,7 @@ export default function UsersDashboardLayout({
                   <DropdownMenuContent className="w-56" align="end" forceMount>
                     <DropdownMenuLabel className="font-normal">
                       <div className="flex flex-col space-y-1">
-                        <p className="text-sm font-medium leading-none">{user.displayName || 'User'}</p>
+                        <p className="text-sm font-medium leading-none">{user.displayName || 'User Divaparadise'}</p>
                         <p className="text-xs leading-none text-muted-foreground">
                           {user.email}
                         </p>
