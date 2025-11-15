2bee330e3581f2df26ac273f8cb442fd4bb4c479
MESY-UNIVERSE <mesy.universe@gmail.com>
2025-11-15 21:02:51 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-3890599599-53972",
-  "appId": "1:73463325437:web:236dc1da097d8a1a07d494",
-  "apiKey": "AIzaSyAI0rs2LasnbJlq6aeDTJ4HxJzQW2-oDEs",
-  "authDomain": "studio-3890599599-53972.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "73463325437"
+  apiKey: "AIzaSyAI0rs2LasnbJlq6aeDTJ4HxJzQW2-oDEs",
+  authDomain: "studio-3890599599-53972.firebaseapp.com",
+  databaseURL: "https://studio-3890599599-53972-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studio-3890599599-53972",
+  storageBucket: "studio-3890599599-53972.appspot.com",
+  messagingSenderId: "73463325437",
+  appId: "1:73463325437:web:236dc1da097d8a1a07d494"
 };
